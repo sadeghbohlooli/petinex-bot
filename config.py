e62e425e5d3768998a0dd26a53b03caa1d2bb035
sadeghbohlooli <sadegh.bohlooli5@gmail.com>
2026-02-24 18:10:31 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "0"))
+BOT_TOKEN = "8350152504:AAH6BcTeMtY4ez8ZJ1Ntoyb1xtHiDGeUDok"
+ADMIN_CHAT_ID = 5008758486
 USER_TIMEOUT = 3600
-DEBUG_MODE = os.environ.get("DEBUG_MODE", "False").lower() == "true"
+DEBUG_MODE = False
