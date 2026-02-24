da6657b58fd78792c31baea718b66d21d2ae2ffa
sadeghbohlooli <sadegh.bohlooli5@gmail.com>
2026-02-24 16:21:35 +0330

    Update states.py

--- core/states.py
+++ core/states.py
@@ -1,11 +1,4 @@
 # core/states.py
-"""تمام stateهای ConversationHandler در یک جا."""
-
-# منوی اصلی
 MAIN_MENU = 0
-
-# فلوی سلامت
 ANSWERING = 1
 MULTI_SELECT = 2
-
-# (در آینده فلوی رژیم و ... اضافه می‌شود)
